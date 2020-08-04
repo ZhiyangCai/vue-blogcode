02c99fd6cdbf8ab040c82d04983bc0d5e7e4af8d
zhiyangcai <2313120974@qq.com>
2020-08-04 16:04:32 +0800

    init

---
+++ src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>© zhiyangcai 2020 wechat: blog01</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
